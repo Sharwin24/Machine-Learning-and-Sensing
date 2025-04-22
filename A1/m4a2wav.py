40c775b85f0e699ce6ccd527beb95e17111b56a7
Sharwin24 <sharwin24@gmail.com>
2025-04-21 19:34:00 -0500

    Function with comments

--- A1/m4a2wav.py
+++ A1/m4a2wav.py
@@ -18,6 +18,31 @@ def generate_dataset_id(name):
 # Enter your name to generate your dataset ID
 dataset_id = generate_dataset_id(name="Sharwin Patil")
 
+
+def m4a_to_wav(file_path: str) -> str:
+    """
+    Convert m4a file to wav file using ffmpeg.
+
+    Args:
+        file_path (str): The path to the m4a file.
+
+    Raises:
+        FileNotFoundError: If the file does not exist
+        ValueError: If the file is not a m4a file
+
+    Returns:
+        str: The path to the converted wav file.
+    """
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"{file_path} does not exist.")
+    if not file_path.endswith(".m4a"):
+        raise ValueError(f"{file_path} is not a m4a file.")
+    wav_file_path = file_path[:-4] + ".wav"
+    os.system(f"ffmpeg -i {file_path} {wav_file_path}")
+    print(f"Converted {file_path} to {wav_file_path}")
+    return wav_file_path
+
+
 # If the folder exists convert the audio files
 if os.path.exists(f"{dataset_id}/"):
     audio_files = os.listdir(f"{dataset_id}")
